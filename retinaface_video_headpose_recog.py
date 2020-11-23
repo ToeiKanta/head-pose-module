544f864bfc0147678fcbb008224abdc9d966ff2a
ToeiKanta <41673034+ToeiKanta@users.noreply.github.com>
2020-11-23 19:18:41 +0700

    refactoring recognition mode faster

--- retinaface_video_headpose_recog.py
+++ retinaface_video_headpose_recog.py
@@ -7,8 +7,13 @@ import os
 from face_detection import RetinaFace
 import headpose_module
 from face_recognition import FaceRecognition_module
+from timer import Timer
+
 
 if __name__ == "__main__":
+    t = Timer()
+    users_in_img = [] # [[name,centerpoint(x,y),box,step]]
+
     # head-pose
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', metavar='FILE', dest='input_file', default=None, help='Input video. If not given, web camera will be used.')
@@ -22,6 +27,7 @@ if __name__ == "__main__":
     # Initialize head pose detection
     hpd = headpose_module.HeadposeDetection(args["landmark_type"], args["landmark_predictor"])
     # close head-pose 
+    isRecognition = True
     filename = './Test/Team.MOV'
     scale = 0.4
     detector = RetinaFace(gpu_id=-1)
@@ -32,8 +38,8 @@ if __name__ == "__main__":
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
     name, ext = osp.splitext(filename)
     out = cv2.VideoWriter(args["output_file"], fourcc, fps, (int(width*scale), int(height*scale)))
-    # if osp.exists(args["output_file"]) and args["fd"]:
-    #     os.remove(args["output_file"])
+    if osp.exists(args["output_file"]) :
+        os.remove(args["output_file"])
 
     # high performance at w: 540.0 h: 960.0
     print(f'w: {width*scale} h: {height*scale}')
@@ -43,8 +49,10 @@ if __name__ == "__main__":
     count = start_frame
     cap.set(cv2.CAP_PROP_POS_FRAMES, count)
     while (cap.isOpened()):
+        t.tic('FF')
         ret, img = cap.read()
-        img = cv2.flip(img,0)
+        if filename == './Test/Team.MOV':
+            img = cv2.flip(img,0)
         # img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         if img is None:
             break
@@ -61,11 +69,10 @@ if __name__ == "__main__":
                 continue
             
             box = box.astype(np.int)
-            # x = box[0]
-            # y = box[1]
-            # w = box[2]
-            # h = box[3]
-            # # head-pose
+            x = box[0]
+            y = box[1]
+            w = box[2]
+            h = box[3]
             # x2 = x-int((w-x)/5)
             # if x2 <= 0: 
             #     x2 = 0
@@ -81,14 +88,42 @@ if __name__ == "__main__":
             # box[3] = h2
 
             # cropped = img[y2:h2,x2:w2]
-            # cv2.imshow('img', cropped)
-            # if cv2.waitKey(1) & 0xFF == ord('q'):
-            #     # headpose_module.t.summary()
-            #     break
-            if (count-start_frame)%3 == 0:
-                face_recognition = FaceRecognition_module()
-                img = face_recognition.detect(frame=img,box=box,landmarks=landmarks, score=score)
-
+            if isRecognition:
+                t.tic('REC')
+                center_point = ((w+x)/2,(h+y)/2)
+                isSamePos = False
+                user_name = ''
+                user_index = 0
+                for users in users_in_img:
+                    nname,bbox,ccenter,step = users[:4]
+                    if step >= 3:
+                        users_in_img.remove(user_index)
+                        user_index += 1
+                        continue
+                    if (center_point[0]>=bbox[0] and center_point[0]<=bbox[2]) and (center_point[1]>=bbox[1] and center_point[1]<=bbox[3]):#(count-start_frame)%1 == 0 :
+                        # use your saving user
+                        users[3] = 0 # reset step
+                        user_name = nname # load save
+                        isSamePos = True
+                        break
+                    user_index += 1
+                if isSamePos:
+                    if user_name == 'unknow':
+                        # if user not founded -> find who is him?
+                        if (count-start_frame)%10 == 0:
+                            face_recognition = FaceRecognition_module()
+                            user_name = face_recognition.detect(frame=img,box=box,landmarks=landmarks, score=score)
+                    else:
+                        users_in_img[user_index] = [user_name,box,center_point,0]
+                elif not isSamePos:
+                    # if new pos -> find who is him?
+                    if (count-start_frame)%10 == 0:
+                        face_recognition = FaceRecognition_module()
+                        user_name = face_recognition.detect(frame=img,box=box,landmarks=landmarks, score=score)
+                        users_in_img.append([user_name,box,center_point,0])
+                cv2.putText(img, user_name, (box[0], box[1] - 5), cv2.FONT_HERSHEY_COMPLEX, 0.7,(255, 255, 255), 2)
+            # print(f'\nuser: {user_name}')
+            # print('REC: %.2f' % t.toc('REC'), end='ms')
             # Display the resulting frame
             frame, angles = hpd.process_image(img,box)
             # width, height = cropped.shape[:2]
@@ -109,8 +144,19 @@ if __name__ == "__main__":
             #     img, (x,y), (w,h), color=(255, 0, 0), thickness=1
             # )
             used_face += 1
+        if isRecognition:
+            i = 0
+            user_list = ''
+            for users in users_in_img:
+                user_list += users[0] + ' '
+                users_in_img[i] = [users[0],users[1],users[2],users[3]+1] # step up
+                i += 1 # count index
+            print(f' users: {user_list} ')
+            
         # print(f'\rused face : {used_face}\n')    
-        print('\rframe: %d' % count, end='')
+        # print('\rframe: %d \n' % count, end='')
+        print('')
+        print('frame: %.2f' % t.toc('FF'), end='ms')
         cv2.imshow('img', img)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             headpose_module.t.summary()
@@ -120,6 +166,7 @@ if __name__ == "__main__":
         # close head-pose
         # if count >= 2100:
         #     break
+        
     # When everything done, release the capture
     cap.release()
     out.release()
